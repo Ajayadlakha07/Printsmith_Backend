839b8c3aed7bf64dc4b2bf9b49adf3f62d86e478
Jogender Saini <Jogender.Saini@Infinityusers.com>
2023-10-09 11:18:24 +0530

    initial commit

--- db.js
+++ db.js
@@ -2,16 +2,14 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 dotenv.config();
 
-const DataBase = process.env.DB_URL
-
 const connectToMongo = () => {
-    mongoose.connect('mongodb+srv://internsinfinity:F63XbnPqJFDtaAIO@cluster0.wfdirt2.mongodb.net/Admin_data', {
+    mongoose.connect('mongodb+srv://rahulchauhah50:gJAShs1TD89ZeWzj@cluster0.qa4bvb8.mongodb.net/Admin_data', {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
+        useUnifiedTopology: true, 
     }).then(() => {
         console.log("conneted to database")
-    }).catch(() => {
-
+    }).catch((err) => {
+        console.log(err)
     })
 }
 
